a413af96e4485e7ffb8e8ad7f5dea170db5320dd
SWQXDBA <76103126+SWQXDBA@users.noreply.github.com>
2020-12-27 00:14:36 +0800

    关于重载操作符
    在实践中，注意到将一个类似i++传给一个&引用时的报错，因为i++返回的是一个temp匿名变量，而普通的&不能接收一个右值
    此处理解为：一般非常量引用是用来修改被引用的对象的。但是如果允许右值赋给引用的话，考虑到右值是一个临时变量，在引用生命期结束之后，这个临时变量就会被销毁，同时所有对它的修改也都将一同消失。这一般来说都是程序中的错误，所以Ｃ++就直接禁止这种行为。常量引用因为不存在修改的问题，所以标准允许将右值赋给常量引用。
    知乎链接：https://www.zhihu.com/question/22439534/answer/21363684
    所以使用const & 常量引用来接收，或者直接传入拷贝；

---
+++ 关于递增操作符的重构与引用接收参数.cpp
@@ -0,0 +1,63 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<string>
+using namespace std;
+class myint
+{
+public:
+	myint() :a(0), b(0)
+	{}
+	myint(int num1, int num2) :a(0), b(0)
+	{
+		a = num1;
+		b = num2;
+	}
+	~myint()
+	{
+		cout << "myint的析构调用" << endl;
+
+
+
+	}
+	int a;
+	int b;
+};
+ostream& operator<<(ostream& ct,  myint &p)
+//或者   ostream& operator<<(ostream& ct,  myint p)
+//这里注意，ostream& operator<<(ostream& ct, const &p)是不行的，因为myint++返回的是一个temp匿名变量，是一个“右值”，普通引用只能接收一个可修改的左值。
+{
+	ct << p.a << p.b << endl;
+	return ct;
+}
+
+
+myint& operator++(myint& aint)
+{
+	aint.a++;
+	aint.b++;
+	return aint;
+
+}
+myint operator++(myint& aint, int)
+{
+	myint temp = aint;
+	aint.a++;
+	aint.b++;
+	return temp;
+}
+void test()
+{
+	myint ps1(2, 5);
+	cout << ps1;
+	cout << ++ps1;
+
+	cout << ps1;
+	cout << "end" << endl;
+}
+int main()
+{
+	test();
+	getchar();
+	getchar();
+
+}
